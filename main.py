4a32f73131a31fcba35e35f5693684eeee98c79d
Martin Sundhaug <martinsundhaug@gmail.com>
2017-06-03 04:56:30 +0200

    Add arguments for speed, wait-time and running once

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import time
 import sys
 import argparse
 
-def delay_print(s):
-    for c in s:
-        sys.stdout.write( '%s' % c )
+def delay_print(string, charsPerSecond):
+    for character in string:
+        sys.stdout.write( '%s' % character )
         sys.stdout.flush()
-        time.sleep(float(1)/float(60))
+        time.sleep(float(1)/float(charsPerSecond))
 
 parser = argparse.ArgumentParser(description='Type out files slowly')
+parser.add_argument('-o', '--once', default=False, dest='once', action='store_true', help='Only go through file-list once')
+parser.add_argument('-s', '--speed', default=60, type=int, help='Characters per second')
+parser.add_argument('-w', '--wait', default=10, type=int, help='Wait between files')
 parser.add_argument('files', metavar='filename', type=str, nargs='+', help='Files to print out')
 args = parser.parse_args()
 
 while True:
     for fName in args.files:
         f=open(fName)
-        delay_print(f.read())
+        delay_print(f.read(), args.speed)
         f.close()
         time.sleep(10)
         sys.stdout.write("\x1b[2J\x1b[H") #Clear screen
+    if args.once:
+        break
